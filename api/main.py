dd1b4dc39fd402dd765fd2222e66799400af948a
Juan Dominguez <Juan.Dominguez@coxautoinc.com>
2025-10-16 12:05:11 -0500

    Avance, conexión con nextcloud aun no funciona

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 import os
+import requests
 import shutil
 from fastapi import FastAPI, File, HTTPException, Depends, UploadFile, status
 from fastapi.middleware.cors import CORSMiddleware
@@ -27,6 +28,7 @@ INVALID_VIDEO_LENGTH = "El video debe tener una duración entre 20 y 60 segundos
 INVALID_VIDEO_RESOLUTION = "La resolución del video debe ser al menos 1080p."
 INVALID_VIDEO_TITLE = "El título del video no puede estar vacío."
 FILE_PROCESSING_ERROR = "Error al procesar el archivo de video."
+FILE_UPLOAD_SUCCESS = "Video subiddo correctamente. Procesamiento en curso."
 
 # Configure logging
 # Configure logging
@@ -164,9 +166,9 @@ async def upload_video(file: UploadFile = File(...), title: str = Form(...)):
     # Save the uploaded file to the temporary path
     with open(temp_filepath, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
-
-    # Validate video length
+    
     try:
+        # Validate video length and resolution
         video_clip = VideoFileClip(temp_filepath)
         duration = video_clip.duration  # Duration in seconds
         width = video_clip.w
@@ -176,9 +178,47 @@ async def upload_video(file: UploadFile = File(...), title: str = Form(...)):
             raise HTTPException(status_code=400, detail=INVALID_VIDEO_LENGTH)
         if width < 1920 or height < 1080:  # Resolution must be at least 1080p
             raise HTTPException(status_code=400, detail=INVALID_VIDEO_RESOLUTION)
+        
+        # Upload video to Nextcloud
+        video_url = upload_video_to_nextcloud(file.file, title)
+        return VideoUploadResponse(
+            message=FILE_UPLOAD_SUCCESS,
+            task_id='1234'
+        )
     except (ValueError, BufferError, RuntimeError) as e:
         logger.error(f"Error processing video file: {e}", exc_info=True)
         raise HTTPException(status_code=500, detail=FILE_PROCESSING_ERROR)
+    finally:
+        # Clean up the temporary file
+        if os.path.exists(temp_filepath):
+            os.remove(temp_filepath)
+    
+
+def upload_video_to_nextcloud(file: File, filename: str) -> str:
+        """
+        Function to upload video to Nextcloud.
+        Return the url of the uploaded video.
+        """
+        try:
+            webdav_url = "http://nextcloud"
+            username = "worker"
+            password = "super-secret"
+            remote_path = f"/raw/{filename}"
+            remote_path_url = webdav_url + f"/remote.php/dav/files{remote_path}"
+            response = requests.put(
+                remote_path_url,
+                data=file,
+                auth=(username, password)
+            )
+            if response.status_code not in [200, 201, 204]:
+                logger.error(f"Failed to upload video to Nextcloud: {response.status_code} - {response.text}")
+                raise HTTPException(status_code=500, detail="Failed to upload video to storage service.")
+            return remote_path
+        except Exception as e:
+            logger.error(f"Failed to upload video to Nextcloud: {e}", exc_info=True)
+            raise HTTPException(status_code=500, detail="Failed to upload video to storage service.")
+
+    # Upload video to Nextcloud (placeholder function)
 
 
 if __name__ == "__main__":
