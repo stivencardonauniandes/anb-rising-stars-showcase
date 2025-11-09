ef0fb0736110bc0c3b2ff7855e121e2a4d153ce5
andres-uniandes <andres-uniandes@andres-macs-MacBook-Pro.local>
2025-11-08 19:20:46 -0500

    Fix double

--- worker/internal/adapters/s3/s3_storage.go
+++ worker/internal/adapters/s3/s3_storage.go
@@ -110,7 +110,8 @@ func (s *S3Storage) Download(ctx context.Context, remotePath string) (io.ReadClo
 }
 
 func (s *S3Storage) Upload(ctx context.Context, remotePath string, data io.Reader) error {
-	key := s.prefix + "/" + remotePath
+	key = strings.ReplaceAll(s.prefix + "/" + remotePath, "//", "/")
+
 	s.logger.Info("uploading to S3",
 		zap.String("bucket", s.bucket),
 		zap.String("key", key),
